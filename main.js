6ed123d2a31f91a64d7d3ebb26fd84afc21798d8
mayank <pm132061@gmail.com>
2025-03-21 15:36:39 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+
+			// // Simple JavaScript to make the accordion functional
+			// document.querySelectorAll(".accordion-header").forEach((button) => {
+			// 	button.addEventListener("click", () => {
+			// 		const accordionItem = button.parentNode;
+
+			// 		// Close all items
+			// 		document.querySelectorAll(".accordion-item").forEach((item) => {
+			// 			if (item !== accordionItem) {
+			// 				item.classList.remove("active");
+			// 			}
+			// 		});
+
+			// 		// Toggle current item
+			// 		accordionItem.classList.toggle("active");
+			// 	});
+			// });
+		
